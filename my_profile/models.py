5151c7bfac7963d7b093df2c9882fd8b19011003
muneer93 <muneerahmed1@icloud.com>
2024-06-10 18:59:37 -0400

    updated html files

--- my_profile/models.py
+++ my_profile/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     city = models.CharField(max_length=50, null=True, blank=True)
@@ -23,6 +22,17 @@ class Profile(models.Model):
 
     class Meta:
         verbose_name_plural = 'Profiles'
+
+
+class ProfileLink(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Profile Links'
 
 
 class Positions(models.Model):
