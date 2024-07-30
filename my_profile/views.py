f64306e6de9bc05e50c2a020916c1ac88f284bac
muneer93 <muneerahmed1@icloud.com>
2024-07-29 21:43:53 -0400

    updated profile.html for dates

--- my_profile/views.py
+++ my_profile/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
 from .models import Profile, Experience
 
-
 def profile(request):
     profile = Profile.objects.first()
-    experience = Experience.objects.all()
+    experiences = Experience.objects.all().order_by('-start_date')  # Sort experiences by start_date descending
     languages = profile.programming_languages.all()
     databases = profile.databases.all()
-    datastack = profile.data_stack.all()
-    cloudstack = profile.cloud_stack.all()
-    framework = profile.web_frameworks.all()
+    data_stack = profile.data_stack.all()
+    cloud_stack = profile.cloud_stack.all()
+    frameworks = profile.web_frameworks.all()
     competencies = profile.competencies.all()
+    
     context = {
         'profile': profile,
-        'experiences': experience,
+        'experiences': experiences,
         'databases': databases,
         'languages': languages,
-        'data_stack': datastack,
-        'cloud_stack': cloudstack,
-        'frameworks': framework,
+        'data_stack': data_stack,
+        'cloud_stack': cloud_stack,
+        'frameworks': frameworks,
         'competencies': competencies
     }
     return render(request, 'my_profile/profile.html', context)
