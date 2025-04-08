bd50ce99c25cc956a49c49e9ea5c713efa39b77d
muneer93 <muneerahmed1@icloud.com>
2025-04-07 22:01:43 -0400

    updated settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -100,6 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
